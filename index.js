812022eba8fc76074a0f370da5399e57227e7bcd
Derrick Nyasio <dnyasio2014@gmail.com>
2022-09-08 16:45:25 +0300

    Added JS functionality

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let form = document.querySelector("form");
-form.addEventListener("submit", handleSubmit());
+function renderHotel(hotel) {
+    let box = document.createElement("div");
+    div.className = "box";
+    box.innerHTML = `
+    <h1>${hotel.name}</h1>
+    <img src="${hotel.imageUrl}" alt = "hotel-picture">
+    <p>${hotel.description}</p>
+    `
+    const btn = document.createElement("button");
+    btn.innerText = "Click me to like";
+    btn.addEventListener("click", () => {
+        const likes = hotel.likes;
+        const likeCounter = document.createElement("p");
+        likeCounter.innerHTML = likes + " likes";
+    })
 
-function handleSubmit(e) {
-
+    box.appendChild(btn);
+    document.querySelector(".boxes .container").appendChild(box)
 }
+
+function getHotelData() {
+    fetch("http://localhost:3000/hotelData")
+        .then(res => res.json())
+        .then(hotelData => console.log(hotelData))
+}
+
+function initialize() {
+    getHotelData();
+    // hotelData.forEach(hotel => renderHotel(hotel))
+}
+
+initialize()
